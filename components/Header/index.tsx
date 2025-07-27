a1722335188f2b4a64827abd33941bfddaeeb823
Paul DiLoreto <soodohh@pm.me>
2025-07-26 23:12:54 -0700

    refactor(sarabeth): mobile navigation

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,40 +9,12 @@ import styles from "./Header.module.css";
 
 const cx = classNames.bind(styles);
 
-type LinkProps = {
-  links: { label: string; url: string }[];
-  onClick?: () => void;
-};
-
-const Links = ({ links, onClick }: LinkProps) => {
-  const route = usePathname();
-  return (
-    <>
-      {links.map((link) => (
-        <Link
-          key={`nav-${link.url}`}
-          href={link.url}
-          className={styles.linkContainer}
-          onClick={onClick}
-        >
-          <span
-            className={cx(styles.link, {
-              activeLink: link.url === route,
-            })}
-          >
-            {link.label}
-          </span>
-        </Link>
-      ))}
-    </>
-  );
-};
-
 type HeaderProps = {
   brandName: string;
 };
 
 const Header = ({ brandName }: HeaderProps) => {
+  const route = usePathname();
   const links = [
     { label: "About", url: "/about" },
     { label: "Engagements", url: "/engagements" },
@@ -59,12 +31,23 @@ const Header = ({ brandName }: HeaderProps) => {
         <SvgLogo className={styles.logoSvg} />
       </Link>
 
-      <nav className={styles.navContainer}>
-        <Links links={links} />
-      </nav>
-
-      <nav className={cx(styles.mobileNavContainer, { mobileNavOpen })}>
-        <Links links={links} onClick={() => setMobileNav(false)} />
+      <nav className={cx(styles.navContainer, { mobileNavOpen })}>
+        {links.map((link) => (
+          <Link
+            key={`nav-${link.url}`}
+            href={link.url}
+            className={styles.linkContainer}
+            onClick={() => setMobileNav(false)}
+          >
+            <span
+              className={cx(styles.link, {
+                activeLink: link.url === route,
+              })}
+            >
+              {link.label}
+            </span>
+          </Link>
+        ))}
       </nav>
 
       <button
